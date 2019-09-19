db313dae0d35f2955340367726e7adfec61eccbc
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-09-19 15:20:47 +0200

    File file_stats.py created

---
+++ src/fabio_rodrigues_ex/ex02/file_stats.py
@@ -0,0 +1,19 @@
+def char_counts(textfilename):
+    pass
+
+
+if __name__ == '__main__':
+
+    filename = 'file_stats.py'
+    frequencies = char_counts(filename)
+    for code in range(256):
+        if frequencies[code] > 0:
+            character = ''
+            if code >= 32:
+                character = chr(code)
+
+            print(
+                '{:3}{:>4}{:6}'.format(
+                    code, character, frequencies[code]
+                )
+            )
