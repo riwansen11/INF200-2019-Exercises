09e8983fb8ff67ef552527bcaf3c49d4ef31df97
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-09-19 13:40:13 +0200

    Exercises ex01 completely done

---
+++ src/fabio_rodrigues_ex/ex01/letter_counts.py
@@ -0,0 +1,16 @@
+def letter_freq(txt):
+    """ Returns a dictionary = {(key : value)}, from the input string, with key = letters and
+    value = the number of occurrences of the letter in the given string.
+    letters = [i for i in text]
+    counter = [text.count(a) for a in text]
+    return dict(zip(letters, counter))
+    """
+    return dict([(i, text.count(i)) for i in txt])
+
+
+if __name__ == '__main__':
+    text = input('Please enter text to analyse: ')
+    frequencies = letter_freq(text)
+
+    for letter, count in frequencies.items():
+        print('{:3}{:10}'.format(letter, count))
