333c493b462b791a6e83e301c31b4c49556b15ea
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-09-23 18:42:29 +0200

    Task B - message_entropy.py - done!

--- src/fabio_rodrigues_ex/ex02/message_entropy.py
+++ src/fabio_rodrigues_ex/ex02/message_entropy.py
@@ -1,17 +1,26 @@
+import math as m
+
+
 def letter_freq(txt):
     """ Returns a dictionary = {(key : value)}, from the input string, with key = letters and
     value = the number of occurrences of the letter in the given string.
     """
-    return dict([(i, text.count(i)) for i in txt])
+    return dict([(i, txt.count(i)) for i in txt.lower()])
 
 
 def entropy(message):
     """
     :param message:
+    N: number of letters in message
+    n_i: number of occurrences of letter i (i is the UTF-8 code for the letter)
+    p_i = n_i/N: frequency of the letter in the message
     :return:
-    the entropy calculated according to the equation above
+    The entropy calculated according to the equation H = - \sum_i p_i \log_2 p_i
     """
-    pass
+    num_letters = len(message)
+    n_i = letter_freq(message)
+    p_i = [n_i[i] / num_letters for i in n_i]
+    return -1 * sum([p_i[i] * m.log2(p_i[i]) for i in range(len(p_i))])
 
 
 if __name__ == "__main__":
