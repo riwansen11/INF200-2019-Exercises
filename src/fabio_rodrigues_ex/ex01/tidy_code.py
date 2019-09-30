d64e89f9a72f609fff8d122b68bdbddb0c68242f
fabiorodp <54884120+fabiorodp@users.noreply.github.com>
2019-09-30 13:12:51 +0200

    Update tidy_code.py
    Fixed # of characters to be according to pep8

--- src/fabio_rodrigues_ex/ex01/tidy_code.py
+++ src/fabio_rodrigues_ex/ex01/tidy_code.py
@@ -5,8 +5,10 @@ __email__ = 'faro@nmbu.no'
 
 
 def game(max_attempts):
-    """It is a game where you have x-max_attempts to guess right the sum of 2 dice.
-    Your score will be the x-max_attempts minus the number of mistakes you had (attempted).
+    """It is a game where you have x-max_attempts 
+    to guess right the sum of 2 dice.
+    Your score will be the x-max_attempts minus the 
+    number of mistakes you had (attempted).
     You lose the game when reach 0 score.
     """
     num_to_guess = sum_dice()
@@ -33,20 +35,21 @@ def sum_dice():
 
 
 def guessed_num():
-    """Ask for the user to guess the sum of 2 rolled dices
+    """Ask for the user to guess the sum of 2 
+    rolled dices.
     """
     return int(input('Guess the sum of 2 dice rolled: '))
 
 
 def guess_check(guessed_number, num_to_guess):
-    """Check if the guessed number is equal to the number to guess,
-    then returns true or false
+    """Check if the guessed number is equal to the 
+    number to guess, then returns true or false.
     """
     return guessed_number == num_to_guess
 
 
 def score(max_attempts, attempted):
-    """Returns the score of the game
+    """Returns the score of the game.
     """
     return max_attempts - attempted
 
