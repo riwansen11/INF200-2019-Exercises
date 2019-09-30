00e9e1167eca5e6264146f135d3890cd3dbcb07a
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-09-30 12:37:59 +0200

    Updated to use with statement when opening the file

--- src/fabio_rodrigues_ex/ex02/file_stats.py
+++ src/fabio_rodrigues_ex/ex02/file_stats.py
@@ -3,13 +3,20 @@ def char_counts(textfilename):
     :param:
     1st. Opens the file with the given filename using encoding utf-8.
     2nd. Reads the entire file content into a single string.
-    3rd. Counts how often each character code (0–255) occurs in the string.
+    3rd. Counts how often each character code (0–255) occurs in the
+    string.
     :return:
-    The result as a list or tuple, where result[i] gives the number of occurrences of character code i.
-    """
+    The result as a list or tuple, where result[i] gives the number of
+    occurrences of character code i.
+
+    old code:
     f = open(textfilename, "r")
     content = f.read()
     f.close()
+    """
+    with open(textfilename, 'r') as f:
+        content = f.read()
+
     return [content.count(i) for i in content]
 
 
