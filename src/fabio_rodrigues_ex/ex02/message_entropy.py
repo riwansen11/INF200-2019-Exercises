86b95cc720b956f6b90d61e291270c7fa34800a1
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-09-30 12:42:41 +0200

    Updated to import only log2 fom math

--- src/fabio_rodrigues_ex/ex02/message_entropy.py
+++ src/fabio_rodrigues_ex/ex02/message_entropy.py
@@ -1,9 +1,10 @@
-import math as m
+from math import log2
 
 
 def letter_freq(txt):
-    """ Returns a dictionary = {(key : value)}, from the input string, with key = letters and
-    value = the number of occurrences of the letter in the given string.
+    """ Returns a dictionary = {(key : value)}, from the input string,
+    with key = letters and value = the number of occurrences of the
+    letter in the given string.
     """
     return dict([(i, txt.count(i)) for i in txt.lower()])
 
@@ -12,7 +13,8 @@ def entropy(message):
     """
     :param message:
     N: number of letters in message
-    n_i: number of occurrences of letter i (i is the UTF-8 code for the letter)
+    n_i: number of occurrences of letter i (i is the UTF-8 code for
+    the letter)
     p_i = n_i/N: frequency of the letter in the message
     :return:
     The entropy calculated according to the equation H
@@ -20,7 +22,7 @@ def entropy(message):
     num_letters = len(message)
     n_i = letter_freq(message)
     p_i = [n_i[i] / num_letters for i in n_i]
-    return -1 * sum([p_i[i] * m.log2(p_i[i]) for i in range(len(p_i))])
+    return -1 * sum([p_i[i] * log2(p_i[i]) for i in range(len(p_i))])
 
 
 if __name__ == "__main__":
