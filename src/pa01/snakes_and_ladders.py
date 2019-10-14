6e8012db3cb7b34c4a8fd6d47637a4e2f798ea95
Fabio Rodrigues Pereira <faro@nmbu.no>
2019-10-14 16:39:35 +0200

    single_game, chutes_and_ladders and create_players functions created

--- src/pa01/snakes_and_ladders.py
+++ src/pa01/snakes_and_ladders.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+from random import randint, seed
+from numpy import median, mean, std
+import matplotlib.pyplot as plt
 
 __author__ = "Fábio Rodrigues Pereira and Rabin"
 __email__ = "faro@nmbu.no and rabin@nmbu.no"
@@ -18,6 +21,39 @@ def single_game(num_players):
     num_moves : int
         Number of moves the winning player needed to reach the goal
     """
+    player = create_players(num_players)
+    has_won = False
+    winner_moves = 0
+    # print(player)
+    # Initiate a game
+    while not has_won:
+        # Initiate a round of the game
+        for p in range(num_players):
+            player[p][0] += randint(1, 6)
+            player[p][1] += 1
+            # print(player)
+            # checks if the players are at a ladder
+            if player[p][0] in chutes_and_ladders():
+                player[p][0] = chutes_and_ladders()[player[p][0]]
+                player[p][1] += 1
+                # print(player)
+            # checks if some player passed the 90th square
+            elif player[p][0] > 90:
+                winner_moves = player[p][1]
+                has_won = True
+                break
+    return winner_moves
+
+
+def chutes_and_ladders():
+    return {
+        1: 40, 8: 10, 36: 52, 43: 62, 49: 79, 65: 82, 68: 85,
+        24: 5, 33: 3, 42: 30, 56: 37, 64: 27, 74: 12, 87: 70
+    }
+
+
+def create_players(num_players):
+    return [[0, 0] for i in range(num_players)]
 
 
 def multiple_games(num_games, num_players):
