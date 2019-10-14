86b03d2a0ba7e4b780c1b392fcb81d4f1252faaa
rabin <rase@nmbu.no>
2019-10-14 16:22:16 +0200

    Name and email corrected

--- src/pa01/chutes.py
+++ src/pa01/chutes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Fábio Rodrigues Pereira and Rabin"
-__email__ = "faro@nmbu.no and rabin@nmbu.no"
+__author__ = "Fábio Rodrigues Pereira and Rabin Senchuri"
+__email__ = "faro@nmbu.no and rase@nmbu.no"
 
